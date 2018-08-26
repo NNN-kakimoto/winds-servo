e8f8e7660b1ffc2d2f08ebdf0529efea1aba7099
NNN <b7080@oic.jp>
2018-08-27 02:06:11 +0900

    #14 不快指数の式までは確認済

--- src/uchiwa_autoswing.py
+++ src/uchiwa_autoswing.py
@@ -26,32 +26,14 @@ def get_discomfort(i2c, address):
     ondo = float(block[4] << 8 | block[5])/10
 
     # 不快指数を算出
-    discomfort = 0.81 * ondo + 0.01 * shitudo (0.99 * ondo - 14.3 ) + 46.3
+    discomfort = (0.81 * ondo) + (0.01 * shitudo) * (0.99 * ondo - 14.3 ) + 46.3
     print(shitudo) # 湿度表示
     print(ondo) # 温度表示
     print(discomfort)
 
     return discomfort
 
-
-# メイン関数
-def main():
-    i2c = smbus.SMBus(1)
-    address = 0x5c
-
-    discomfort = get_discomfort(i2c, address)
-    #ここまで書いた
-
-    # GPIOのモード設定
-    GPIO.setmode(GPIO.BCM)
-
-    # GPIO18を制御パルスの出力に設定
-    gp_out = 18
-    GPIO.setup(gp_out, GPIO.OUT)
-
-    # サーボの制御パルスと周波数の設定
-    servo = GPIO.PWM(gp_out, 50)
-
+def move_servo(servo)
     # パルス出力の開始
     servo.start(0)
 
@@ -63,6 +45,32 @@ def main():
 
     # 後処理
     servo.stop()
+
+# メイン関数
+def main():
+    i2c = smbus.SMBus(1)
+    address = 0x5c
+    
+    # GPIOのモード設定
+    GPIO.setmode(GPIO.BCM)
+
+    # GPIO18を制御パルスの出力に設定
+    gp_out = 18
+    GPIO.setup(gp_out, GPIO.OUT)
+
+    # サーボの制御パルスと周波数の設定
+    servo = GPIO.PWM(gp_out, 50)
+
+    # 以下、無限ループ
+    while true:
+        discomfort = get_discomfort(i2c, address)
+
+        if(discomfort > 75) # 不快指数のしきい値を一度75とする
+            #print('disconfort')
+            move_servo(servo)
+
+        
+    
     GPIO.cleanup()
 
 if __name__ == '__main__':
